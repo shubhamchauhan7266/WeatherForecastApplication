b48e45fcae2f9e5e730a156d5addb0b6964bba8e
Shubham Chauhan <shubham.chauhan@kelltontech.com>
2019-03-01 02:22:08 +0530

    + initial commit

--- app/src/main/java/com/weatherforecastapplication/database/entity/DailyWeatherForecast.java
+++ app/src/main/java/com/weatherforecastapplication/database/entity/DailyWeatherForecast.java
@@ -1,6 +1,14 @@
 package com.weatherforecastapplication.database.entity;
 
+import android.arch.persistence.room.Entity;
+
 import java.io.Serializable;
 
+/**
+ * This Entity class is used to create a table for Daily Weather Forecast Details.
+ *
+ * @author Shubham Chauhan
+ */
+@Entity(tableName = "daily_weather_forecast")
 public class DailyWeatherForecast implements Serializable {
 }
--- app/src/main/java/com/weatherforecastapplication/database/entity/HourWeatherForecast.java
+++ app/src/main/java/com/weatherforecastapplication/database/entity/HourWeatherForecast.java
@@ -1,7 +1,15 @@
 package com.weatherforecastapplication.database.entity;
 
+import android.arch.persistence.room.Entity;
+
 import java.io.Serializable;
 
+/**
+ * This Entity class is used to create a table for Hourly Weather Forecast Details.
+ *
+ * @author Shubham Chauhan
+ */
+@Entity(tableName = "hourly_weather_forecast")
 public class HourWeatherForecast implements Serializable {
 
 
